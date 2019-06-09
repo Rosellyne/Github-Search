d951f88f5417bdbc5d968a2dc7066faa9f3cb149
Rosellyne <elijahroselyne@gmail.com>
2019-06-10 02:08:30 +0300

    added the panel heading to the search component.html

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+profile: any[];
   constructor(private profileservice: ProfileService) {
     this.profileservice.getProfile().subscribe(profile => {
       console.log(profile);
+      this.profile = profile;
     });
   }
 
