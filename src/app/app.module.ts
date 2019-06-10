733eeeacab43071bb252b02dc06ad0f585fae8bf
Rosellyne <elijahroselyne@gmail.com>
2019-06-10 04:28:50 +0300

    add the imported routingmodule to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule} from '@angular/forms';
+import {RoutingModule} from './routing/routing.module';
 
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { SearchComponent } from './search/search.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RoutingModule
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
