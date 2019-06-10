e4020e2b726a1c1485c65b84c143ead7119d3ffd
Rosellyne <elijahroselyne@gmail.com>
2019-06-10 14:17:25 +0300

    Auto-generated commit

--- src/app/lastname.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LastnamePipe } from './lastname.pipe';
-
-describe('LastnamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new LastnamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
